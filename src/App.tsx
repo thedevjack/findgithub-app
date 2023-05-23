54a53122f06169f450f35ea8468b25110850c641
thedevjack <jackson1santos@hotmail.com>
2023-05-23 16:48:52 -0300

    Componentes e estilização finalizada em 80%

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,17 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+
+import classes from './App.module.css';
 
 function App() {
-  
-
   return (
     <>
-      <div>
-        <h1>
+      <div className={classes.app}>
+        <h1>GitHub Finder</h1>
 
-          GitHub Finder
-        </h1>
-
-        <Outlet></Outlet>
+        <Outlet />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Error/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Error() {
+    return(
+
+<p>Usuário não encontrado</p>
+    )
+}
+
+export default Error;
